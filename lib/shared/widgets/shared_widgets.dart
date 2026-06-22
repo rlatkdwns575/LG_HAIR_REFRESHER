61af8a92b2f8132edea09a5bdf4f9e55f751a026
rlatkdwns575 <wb001223@gmail.com>
2026-06-22 11:15:24 +0900

    chore(shared): 미사용 app_box_mini_button 위젯 제거

--- lib/shared/widgets/shared_widgets.dart
+++ lib/shared/widgets/shared_widgets.dart
@@ -3,7 +3,6 @@ export 'app_battery_status.dart';
 export 'app_bottom_button_bar.dart';
 export 'app_fixed_bottom_button_area.dart';
 export 'app_box_button.dart';
-export 'app_box_mini_button.dart';
 export 'app_calendar_day_strip.dart';
 export 'app_calendar_item.dart';
 export 'app_calendar_top_header.dart';
